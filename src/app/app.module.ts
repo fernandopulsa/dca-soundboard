6985535cd30187ef150f16c8f854adf84784fd32
Fernando Pulsa <fernandopulsa@gmail.com>
2018-05-24 21:23:48 +0200

    first components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SoundboardComponent } from './soundboard/soundboard.component';
+import { SoundboardItemComponent } from './soundboard/soundboard-item/soundboard-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SoundboardComponent,
+    SoundboardItemComponent
   ],
   imports: [
     BrowserModule
