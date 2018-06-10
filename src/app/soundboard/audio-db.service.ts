be84ce35a1a55a90128b91341111ff40a57a91d6
fernandofts <fernandofts12@gmail.com>
2018-06-10 13:33:42 +0200

    Start to create JSON with the audios

--- src/app/soundboard/audio-db.service.ts
+++ src/app/soundboard/audio-db.service.ts
@@ -8,7 +8,189 @@ import { AudioElement } from './audio.model';
 export class AudioDbService {
   
   private audios:AudioElement[] = [
-    new AudioElement('test1', '/assets/audios/musqueta_J_1.wav')
+    new AudioElement(
+      'Alerta Spoiler',
+      '/assets/audios/alerta-spoiler.wav',
+      'spoiler',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'Ciencia',
+      '/assets/audios/ciencia_C_1.wav',
+      'ciencia',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'Ciencia',
+      '/assets/audios/ciencia_CFJ_1.wav',
+      'ciencia',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'Ciencia',
+      '/assets/audios/ciencia_F_1.wav',
+      'ciencia',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'Ciencia',
+      '/assets/audios/ciencia_J_1.wav',
+      'ciencia',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'De Camino A',
+      '/assets/audios/decaminoa_C_1.wav',
+      'De Camino A',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'De Camino A',
+      '/assets/audios/decaminoa_CFJ_1.wav',
+      'De Camino A',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'Empotrarte',
+      '/assets/audios/emprotarte.wav',
+      'Empotrarte',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'Entrevista',
+      '/assets/audios/entrevista_C_1.wav',
+      'Entrevista',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'Entrevista',
+      '/assets/audios/entrevista_C_2.wav',
+      'Entrevista',
+      'Carlos',
+      '02'
+    ),
+    new AudioElement(
+      'Entrevista',
+      '/assets/audios/entrevista_F_1.wav',
+      'Entrevista',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'Entrevista',
+      '/assets/audios/entrevista_F_2.wav',
+      'Entrevista',
+      'Fer',
+      '02'
+    ),
+    new AudioElement(
+      'Entrevista',
+      '/assets/audios/entrevista_F_3.wav',
+      'Entrevista',
+      'Fer',
+      '03'
+    ),
+    new AudioElement(
+      'Entrevista',
+      '/assets/audios/entrevista_J_1.wav',
+      'Entrevista',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'Entrevista',
+      '/assets/audios/entrevista_J_2.wav',
+      'Entrevista',
+      'Jordi',
+      '02'
+    ),
+    new AudioElement(
+      'Entrevista',
+      '/assets/audios/entrevista_J_3.wav',
+      'Entrevista',
+      'Jordi',
+      '03'
+    ),
+    new AudioElement(
+      'El Filmmm',
+      '/assets/audios/filmmm_J_1.wav',
+      'peliculas',
+      'Jordi',
+      '03'
+    ),
+    new AudioElement(
+      'Future is now',
+      '/assets/audios/futurenow_C_1.wav',
+      'futurenow',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'Future is now',
+      '/assets/audios/futurenow_C_2-wei.wav',
+      'futurenow',
+      'Carlos',
+      '02'
+    ),
+    new AudioElement(
+      'Future is now',
+      '/assets/audios/futurenow_F_1.wav',
+      'futurenow',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'Future is now',
+      '/assets/audios/futurenow_J_1.wav',
+      'futurenow',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'Future is now',
+      '/assets/audios/futurenow_J_2.wav',
+      'futurenow',
+      'Jordi',
+      '02'
+    ),
+    new AudioElement(
+      'La seccion',
+      '/assets/audios/la_seccion.wav',
+      'La seccion',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'Le llamariamos Bukake',
+      '/assets/audios/le_llamariamos_bukake.wav',
+      'bukake',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'El Filmmm',
+      '/assets/audios/lefilme_F_1.wav',
+      'peliculas',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'Lo Recomiendo',
+      '/assets/audios/lo_recomiendo.wav',
+      'Lo recomiendo',
+      'Fer',
+      '01'
+    ),
+
   ];
 
   constructor() { }
@@ -16,7 +198,5 @@ export class AudioDbService {
   getAudios(){
     return this.audios;
   }
-
-
 
 }
--- src/app/soundboard/audio.model.ts
+++ src/app/soundboard/audio.model.ts
@@ -3,11 +3,14 @@ export class AudioElement {
     constructor(
         public name: string,
         public url: string,
-        // public section: string,
-        // public voice: string,
-        // public take: string,
+        public section: string,
+        public voice: string,
+        public take: string,
     ) {
         this.name = name;
         this.url = url;
+        this.section = section;
+        this.voice = voice;
+        this.take = take;
     }
 }
