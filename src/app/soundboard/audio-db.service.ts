420c918a0cae6ded8ee286387d6cc9e513fbe050
Fernando Pulsa <fernandopulsa@gmail.com>
2018-06-19 13:52:30 +0200

    Audio DB added in a JSON

--- src/app/soundboard/audio-db.service.ts
+++ src/app/soundboard/audio-db.service.ts
@@ -190,7 +190,433 @@ export class AudioDbService {
       'Fer',
       '01'
     ),
-
+    new AudioElement(
+      'Me corro en tu cara',
+      '/assets/audios/me_corro_en_tu_cara.wav',
+      'Me corro en tu cara',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'Musiqueta de la buena',
+      '/assets/audios/musqueta_F_1.wav',
+      'musica',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'Musiqueta de la buena',
+      '/assets/audios/musqueta_F_2.wav',
+      'musica',
+      'Fer',
+      '02'
+    ),
+    new AudioElement(
+      'Musiqueta de la buena',
+      '/assets/audios/musqueta_F_3.wav',
+      'musica',
+      'Fer',
+      '03'
+    ),
+    new AudioElement(
+      'Musiqueta de la buena',
+      '/assets/audios/musqueta_J_1.wav',
+      'musica',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'Musiqueta de la buena',
+      '/assets/audios/musqueta_J_2.wav',
+      'musica',
+      'Jordi',
+      '02'
+    ),
+    new AudioElement(
+      'Netflix',
+      '/assets/audios/netflix_F_1.wav',
+      'netflix',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'La Pelicula',
+      '/assets/audios/pelicula_C_1.wav',
+      'peliculas',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'La Pelicula',
+      '/assets/audios/pelicula_F_1.wav',
+      'peliculas',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'La Pelicula',
+      '/assets/audios/pelicula_J_1.wav',
+      'peliculas',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'La Pelicula',
+      '/assets/audios/pelicula_FJ_1.wav',
+      'peliculas',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'Podcasting',
+      '/assets/audios/podcasting_C_1.wav',
+      'Podcasting',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'Podcasting',
+      '/assets/audios/podcasting_F_1.wav',
+      'Podcasting',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'Podcasting',
+      '/assets/audios/podcasting_F_2.wav',
+      'Podcasting',
+      'Fer',
+      '02'
+    ),
+    new AudioElement(
+      'La Psicologia',
+      '/assets/audios/psico_C_1.wav',
+      'psicologia',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'La Psicologia',
+      '/assets/audios/psico_C_2.wav',
+      'psicologia',
+      'Carlos',
+      '02'
+    ),
+    new AudioElement(
+      'La Psicologia',
+      '/assets/audios/psico_F_1.wav',
+      'psicologia',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'La Psicologia',
+      '/assets/audios/psico_J_1.wav',
+      'psicologia',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'La Psicologia',
+      '/assets/audios/psico_J_2.wav',
+      'psicologia',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'La Psicologia',
+      '/assets/audios/psico_FC_1.wav',
+      'psicologia',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'Un recomendar',
+      '/assets/audios/recomendar_C_1.wav',
+      'Un recomendar',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'Un recomendar',
+      '/assets/audios/recomendar_C_2.wav',
+      'Un recomendar',
+      'Carlos',
+      '02'
+    ),
+    new AudioElement(
+      'Un recomendar',
+      '/assets/audios/recomendar_F_1.wav',
+      'Un recomendar',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'Un recomendar',
+      '/assets/audios/recomendar_F_2.wav',
+      'Un recomendar',
+      'Fer',
+      '02'
+    ),
+    new AudioElement(
+      'Review',
+      '/assets/audios/review_C_1.wav',
+      'Review',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'Review',
+      '/assets/audios/review_C_2.wav',
+      'Review',
+      'Carlos',
+      '02'
+    ),
+    new AudioElement(
+      'Review',
+      '/assets/audios/review_C_3.wav',
+      'Review',
+      'Carlos',
+      '03'
+    ),
+    new AudioElement(
+      'Review',
+      '/assets/audios/review_J_1.wav',
+      'Review',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion Alba',
+      '/assets/audios/secion_Alba_C_1.wav',
+      'La seccion',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion Alba',
+      '/assets/audios/secion_Yas_J_2.wav',
+      'La seccion',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion Alba',
+      '/assets/audios/secion_Yas_CF_1.wav',
+      'La seccion',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion carlos',
+      '/assets/audios/secion_carlos_C_1.wav',
+      'La seccion',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion carlos',
+      '/assets/audios/secion_carlos_C_2.wav',
+      'La seccion',
+      'Carlos',
+      '02'
+    ),
+    new AudioElement(
+      'La seccion carlos',
+      '/assets/audios/secion_carlos_F_1.wav',
+      'La seccion',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion carlos',
+      '/assets/audios/secion_carlos_J_1.wav',
+      'La seccion',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion Fernando',
+      '/assets/audios/secion_fernando_F_1.wav',
+      'La seccion',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion fresquer',
+      '/assets/audios/secion_fresquer_C_1.wav',
+      'La seccion',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion fresquer',
+      '/assets/audios/secion_fresquer_F_1.wav',
+      'La seccion',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion fresquer',
+      '/assets/audios/secion_fresquer_J_1.wav',
+      'La seccion',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion huevos colgando',
+      '/assets/audios/secion_Yas_C_1.wav',
+      'La seccion',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion Yas',
+      '/assets/audios/secion_Yas_C_2.wav',
+      'La seccion',
+      'Carlos',
+      '02'
+    ),
+    new AudioElement(
+      'La seccion Yas',
+      '/assets/audios/secion_Yas_C_3.wav',
+      'La seccion',
+      'Carlos',
+      '03'
+    ),
+    new AudioElement(
+      'La seccion Yas',
+      '/assets/audios/secion_Yas_F_1.wav',
+      'La seccion',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'La seccion Yas',
+      '/assets/audios/secion_Yas_J_1.wav',
+      'La seccion',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'Un sensuar',
+      '/assets/audios/sensuar_C_1.wav',
+      'Un sensuar',
+      'Carlos',
+      '01'
+    ),
+    new AudioElement(
+      'Un sensuar',
+      '/assets/audios/sensuar_C_2.wav',
+      'Un sensuar',
+      'Carlos',
+      '02'
+    ),
+    new AudioElement(
+      'Un sensuar',
+      '/assets/audios/sensuar_F_1.wav',
+      'Un sensuar',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'Un sensuar',
+      '/assets/audios/sensuar_J_1.wav',
+      'Un sensuar',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'Un sensuar',
+      '/assets/audios/sensuar_J_2.wav',
+      'Un sensuar',
+      'Jordi',
+      '02'
+    ),
+    new AudioElement(
+      'Series',
+      '/assets/audios/series_C_1.wav',
+      'Series',
+      'Carlos',
+      '02'
+    ),
+    new AudioElement(
+      'Series',
+      '/assets/audios/series_C_2.wav',
+      'Series',
+      'Carlos',
+      '02'
+    ),
+    new AudioElement(
+      'Series',
+      '/assets/audios/series_CJ_1.wav',
+      'Series',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'Series',
+      '/assets/audios/series_F_1.wav',
+      'Series',
+      'Fer',
+      '01'
+    ),
+    new AudioElement(
+      'Series',
+      '/assets/audios/series_J_1.wav',
+      'Series',
+      'Jordi',
+      '01'
+    ),
+    new AudioElement(
+      'Series',
+      '/assets/audios/series_J_2.wav',
+      'Series',
+      'Jordi',
+      '02'
+    ),
+    new AudioElement(
+      'Test Drogas',
+      '/assets/audios/test_drogas.wav',
+      'Test',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'Test embarazo',
+      '/assets/audios/test_embarazo.wav',
+      'Test',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'El Test',
+      '/assets/audios/test_F_1.wav',
+      'Test',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'Movimiento Sexy',
+      '/assets/audios/movimento_sexy.wav',
+      'Movimiento Sexy',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'Ya vas tarde',
+      '/assets/audios/ya_tarde_caballero.wav',
+      'Ya vas tarde',
+      'group',
+      '01'
+    ),
+    new AudioElement(
+      'Ya Wey!',
+      '/assets/audios/ya_wey.wav',
+      'Ya Wey!',
+      'group',
+      '01'
+    ),
   ];
 
   constructor() { }
